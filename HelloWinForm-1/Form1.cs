8bc0d44e470b9472f39b10c318443683491de9de
djt <david@nekstep.com>
2014-03-09 16:57:34 -0400

    First version

---
+++ HelloWinForm-1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloWinForm_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            label2.Text = Width + "x" + Height + "  :  " + ClientSize.Width + "/" + ClientSize.Height;
+            centerControl(label1);
+        }
+
+        private void centerControl(Control ctl)
+        {
+            ctl.Left = (ClientSize.Width / 2) - (ctl.Width / 2);
+            ctl.Top = (ClientSize.Height / 2) - (ctl.Height / 2);
+        }
+
+    }
+}
